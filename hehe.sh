4293c81f175a8942a90d978e3bdd20b769fb884b
FarelZeus <63641611+FarelZeus@users.noreply.github.com>
2020-07-15 06:12:48 +0000

    Create hehe.sh
    MoonXploit

---
+++ hehe.sh
@@ -0,0 +1,12 @@
+clear
+echo "SABAR YA PALKON ! "
+sleep 2
+pkg update && pkg upgrade
+pkg install git
+pkg install wget
+pkg install python2
+pkg install figlet
+pkg install toilet
+sleep 2
+echo "MAKASIH YA AJG"
+login
